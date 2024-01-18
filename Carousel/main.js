86f06b2be69dd7151392e67849bbbf876fea54cc
mahaburio <nmbiswas256@gmail.com>
2024-01-19 02:33:10 +0600

    carousel

--- Carousel/main.js
+++ Carousel/main.js
@@ -1,14 +1,22 @@
-
 //  ? ========== Card JS ==========
+
+  /* 
+ TODO ========== Radio Button ========== */
+
+let radioOne = document.getElementById("radioOne");
+let radioTwo = document.getElementById("radioTwo");
+let radioThree = document.getElementById("radioThree");
+
+/* 
+ TODO ========== Prev/Next Button ========== */
 
 let prevBtn = document.getElementById("prevBtn");
 let nextBtn = document.getElementById("nextBtn");
+/* 
+ TODO ========== Card ========== */
 
 let cardSec = document.getElementById("cardSec");
 
-let personOne = document.getElementById("personOne");
-let personTwo = document.getElementById("personTwo");
-let personThree = document.getElementById("personThree");
 
 let isClick = 0;
 
@@ -16,19 +24,78 @@ nextBtn.addEventListener("click", function () {
   isClick++;
   if (isClick == 1) {
     cardSec.style.transform = "translateX(-33%)";
+
+    radioTwo.classList.add("active");
+
+    radioOne.classList.remove("active");
+    radioThree.classList.remove("active");
+
     prevBtn.addEventListener("click", function () {
       cardSec.style.transform = "translateX(-0%)";
       isClick = 0;
+
+      radioOne.classList.add("active");
+
+      radioThree.classList.remove("active");
+      radioTwo.classList.remove("active");
     });
   } else if (isClick == 2) {
     cardSec.style.transform = "translateX(-66.5%)";
+
+    radioThree.classList.add("active");
+
+    radioTwo.classList.remove("active");
+    radioOne.classList.remove("active");
+
     prevBtn.addEventListener("click", function () {
       cardSec.style.transform = "translateX(-33%)";
+
+      radioTwo.classList.add("active");
+
+      radioOne.classList.remove("active");
+      radioThree.classList.remove("active");
+
       prevBtn.addEventListener("click", function () {
         cardSec.style.transform = "translateX(-0%)";
+
+        radioOne.classList.add("active");
+
+        radioTwo.classList.remove("active");
+        radioThree.classList.remove("active");
       });
     });
   } else {
     isClick === 0;
   }
 });
+
+radioTwo.addEventListener("click", function () {
+  cardSec.style.transform = "translateX(-33%)";
+
+  radioTwo.classList.add("active");
+
+  radioOne.classList.remove("active");
+  radioThree.classList.remove("active");
+});
+
+radioThree.addEventListener("click", function () {
+  cardSec.style.transform = "translateX(-66%)";
+
+  radioThree.classList.add("active");
+
+  radioOne.classList.remove("active");
+  radioTwo.classList.remove("active");
+
+
+});
+
+radioOne.addEventListener("click", function () {
+  cardSec.style.transform = "translateX(0%)";
+
+  radioOne.classList.add("active");
+
+  radioThree.classList.remove("active");
+  radioTwo.classList.remove("active");
+});
+
+
---
+++ codemama/main.js
@@ -0,0 +1,11 @@
+let x = Number(prompt('Enter Number'))
+let pi = 3.14;
+
+let res = pi*(Math.pow(x,2))
+let result = res.toFixed(2)
+console.log (`The area of the circle is ${result} square units.`)
+
+
+
+
+
